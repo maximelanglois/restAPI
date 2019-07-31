e0808816144045cc028bb8b7a7d58dacd57eb88b
champagnem <mathieu.champagne@gmail.com>
2019-07-31 17:19:52 -0400

    get col status

--- Controllers/ColumnsController.cs
+++ Controllers/ColumnsController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using restapi.Models;
+using System.Linq;
 
 namespace restapi.Controllers
 {
@@ -16,7 +17,9 @@ namespace restapi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<column>> GetColumns()
         {
-            return _context.columns;
+            // return _context.columns;
+            var everyColumns = _context.columns;
+            return everyColumns;
         }
 
         [HttpGet("{id}")]
@@ -28,8 +31,39 @@ namespace restapi.Controllers
             {
                 return NotFound();
             }
+            
             return column;
         }
+
+        [HttpGet("{id}/status")]
+        public ActionResult<string> GetColumnStatus(int id)
+        {
+            var columnStatus = _context.columns.Find(id).status;
+            
+            if (columnStatus == null)
+            {
+                return NotFound();
+            }
+            
+            return columnStatus;
+        }
+
+        [HttpGet("inactive")]
+        public ActionResult<IEnumerable<column>> GetInactiveColumns()
+        {
+            var inactiveColumns = _context.columns.Where(c => (c.status != "active")).ToList();
+
+            
+            if (inactiveColumns == null)
+            {
+                return NotFound();
+            }
+            
+            return inactiveColumns;
+        }
+
+      
+
     }
 
 }
